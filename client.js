709c3030136bbda767cc6a33568528f7ed4ec2b3
Calebe Brim <calebebrim@gmail.com>
2023-05-02 11:20:55 -0300

    last updates

--- client.js
+++ client.js
@@ -1,9 +1,32 @@
 const net = require('net');
 var readlineSync = require('readline-sync');
 
+const client = net.createConnection({host:'localhost', port: 8124 }, () => {
+    return new Promise(() => { 
+        console.log('connected to server!');
+        client.on('data', (data) => {
+            console.log(data.toString());
+        });
+        
+        client.on('end', () => {
+            console.log('disconnected from server');
+        });
+        readlineTsk(client);
+    })
+});
 
-const socket = net.connect(8124, "localhost", (event)=>{
-    console.log("connected", event)
 
-})
+
+
+function readlineTsk(c) {
+    var line = readlineSync.question('send: ');
+
+    if (line == 'c' ) {
+        return;
+    } else {
+        c.write(line);
+    }
+
+    setTimeout(()=>readlineTsk(c), 10)
+}
 
--- server.js
+++ server.js
@@ -1,24 +1,45 @@
+var readlineSync = require("readline-sync");
 
 // 192.168.10.111
-const net = require('net');
-const readline = require('node:readline');
+const net = require("net");
 
 const server = net.createServer((c) => {
-  // 'connection' listener.
-  console.log('client connected');
-  c.on('end', () => {
-    console.log('client disconnected');
+  return new Promise(() => {
+    console.log("client connected");
+    c.on("end", () => {
+      console.log("client disconnected");
+    });
+
+    c.on("data", (data) => {
+      console.log("client: ", data.toString());
+    });
+
+    c.write("bem vindo!");
+
+    readlineTsk(c);
   });
-  c.write('hello\r\n');
-  c.pipe(c);
-
-
 });
-server.on('error', (err) => {
+
+server.on("error", (err) => {
   throw err;
 });
+
 server.listen(8124, () => {
-  console.log('server bound');
+  console.log("server bound");
 });
 
-console.log("test")
+function readlineTsk(c) {
+  return new Promise(() => {
+    // var line = readlineSync.question('send: ');
+    for (let index = 0; index < array.length; index++) {
+      c.write(index);
+    }
+    // if (line == "c") {
+    //   return;
+    // } else {
+    //   c.write(line);
+    // }
+
+    setTimeout(() => readlineTsk(c), 1000);
+  });
+}
